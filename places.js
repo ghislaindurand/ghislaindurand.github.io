36b645de4bb7c32878cec65b6958a9ae4d04aafe
ghislaindurand <ghislain.durand@gmail.com>
2019-12-10 00:02:23 +0100

    modif wiki

--- places.js
+++ places.js
@@ -21,6 +21,11 @@ const loadPlaces = function (coords) {
 
 // getting places from REST APIs
 function loadPlaceFromAPIs(position) {
+
+
+  
+
+  /*
   const params = {
       radius: 300,    // search places not farther than this value (in meters)
       clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
@@ -48,8 +53,103 @@ function loadPlaceFromAPIs(position) {
       })
       .catch((err) => {
           console.error('Error with places API', err);
-      })
+      })*/
 };
+function wikiUrl(path, api, mobile) {
+  const wikiTag = 'fr';
+  let url = 'https://' + wikiTag;
+  if (mobile) url += '.m';
+  return url + '.wikipedia.org/' + (api ? 'w/api.php?' : 'wiki/') + path
+}
+
+async function getNearbyArticle(position) {
+  console.info('Finding nearby article');
+  const response = await fetchWithTimeout(wikiUrl('action=query&format=json&origin=*&generator=geosearch&ggsradius=10000&ggsnamespace=0&ggslimit=50&formatversion=2&ggscoord=' + encodeURIComponent(position.latitude) + '%7C' + encodeURIComponent(position.longitude), true, true));
+  if (!response.ok) {
+    console.error('Wikipedia nearby failed', response)
+    throw new Error('Wikipedia nearby is down');
+  }
+  const json = await response.json();
+  console.info('Nearby response', json);
+  const pages = json.query.pages;
+  let places = [];
+  for (let page of pages) {
+    const title = page.title;
+    //if (seen[title]) {
+    //  continue;
+    //}
+    //seen[title] = true;
+    console.info('Title', title);
+    places.push(await getContent(title));
+    if (places.length >= 5) break;
+  }
+  return places;
+  //return null;
+}
+
+async function getContent(title) {
+  console.info('Getting content');
+  const response = await fetchWithTimeout(wikiUrl('redirects=true&format=json&origin=*&action=query&prop=extracts|coordinates&titles=' + encodeURIComponent(title), true));
+  if (!response.ok) {
+    console.error('Wikipedia content call failed', response)
+    throw new Error('Wikipedia content is down');
+  }
+  const json = await response.json();
+  const page = Object.values(json.query.pages)[0];
+  console.info('Page', page)
+  //seen[page.title] = true;
+  return {
+    url: wikiUrl(encodeURIComponent(page.title), false),
+    title: page.title,
+    label: page.title,
+    name: page.title,
+    content: simpleHtmlToText(page.extract.trim()),
+    //lang: state.lang.speechTag,
+    /*
+    coordinates: page.coordinates[0] ? {
+      lat: page.coordinates[0].lat,
+      lng: page.coordinates[0].lon,
+    } : null,*/
+    location: page.coordinates[0] ? {
+      lat: page.coordinates[0].lat,
+      lng: page.coordinates[0].lon,
+    } : null,
+  };
+}
+
+function timeout(time, message) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => reject(new Error('Timeout: ' + message)), time);
+  })
+}
+
+function fetchWithTimeout(url, paras) {
+  return Promise.race([fetch(url, paras), 
+                       timeout(15 * 1000, 'Fetch timed out for ' + url)]);
+}
+
+function simpleHtmlToText(html) {
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  remove(div.querySelector('#References'));
+  remove(div.querySelector('#See_also'));
+  let text = div.textContent;
+  text = text.replace(/(.)\n/g, '$1.\n');
+  // Remove stuff in parantheses. Nobody wants to hear that stuff.
+  // This isn't how you pass the Google interview.
+  // But it is technically O(n)
+  for (let i = 0; i < 10; i++) {
+    text = text.replace(/\([^\)]+\)/g, '');
+  }
+  return text;
+}
+
+function remove(element) {
+  if (!element) {
+    return false;
+  }
+  return element.parentElement.removeChild(element);
+}
 
 
 window.onload = () => {
@@ -59,8 +159,9 @@ window.onload = () => {
   return navigator.geolocation.getCurrentPosition(function (position) {
 
       // than use it to load from remote APIs some places nearby
-      loadPlaces(position.coords)
-          .then((places) => {
+      //loadPlaces(position.coords)
+      getNearbyArticle(position.coords)
+                .then((places) => {
               places.forEach((place) => {
                   const latitude = place.location.lat;
                   const longitude = place.location.lng;
