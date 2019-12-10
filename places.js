aee649201d170df8b4548741ff71c243c74cb35e
ghislaindurand <ghislain.durand@gmail.com>
2019-12-11 00:24:01 +0100

    icon

--- places.js
+++ places.js
@@ -154,6 +154,50 @@ window.onload = () => {
 };*/
 
 
+function renderIcon(place) {
+  let scene = document.querySelector('a-scene');
+
+      const latitude = place.location.lat;
+      const longitude = place.location.lng;
+
+      // add place icon
+      const icon = document.createElement('a-image');
+      icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+      icon.setAttribute('name', place.name);
+      icon.setAttribute('src', place.image);
+
+      // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+      icon.setAttribute('scale', '20, 20');
+
+      icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+      const clickListener = function (ev) {
+          ev.stopPropagation();
+          ev.preventDefault();
+
+          const name = ev.target.getAttribute('name');
+
+          const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+          if (el && el === ev.target) {
+              const label = document.createElement('span');
+              const container = document.createElement('div');
+              container.setAttribute('id', 'place-label');
+              label.innerText = name;
+              container.appendChild(label);
+              document.body.appendChild(container);
+
+              setTimeout(() => {
+                  container.parentElement.removeChild(container);
+              }, 1500);
+          }
+      };
+
+      icon.addEventListener('click', clickListener);
+
+      scene.appendChild(icon);
+}
+
 AFRAME.registerComponent('geoloc', {
   init: function () {
     // Code here.
@@ -167,6 +211,9 @@ AFRAME.registerComponent('geoloc', {
       getNearbyArticle(position.coords)
                 .then((places) => {
               places.forEach((place) => {
+
+                renderIcon(place);
+/*
                   const latitude = place.location.lat;
                   const longitude = place.location.lng;
 
@@ -184,6 +231,7 @@ AFRAME.registerComponent('geoloc', {
                   //});
 
                   scene.appendChild(text);
+                  */
               });
           })
   },
