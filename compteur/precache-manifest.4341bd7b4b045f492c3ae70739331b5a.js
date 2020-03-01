e7669008f862191b2361d0990fec3d4b84d267aa
ghislaindurand <ghislain.durand@gmail.com>
2020-03-02 00:33:09 +0100

    compteur bio

---
+++ compteur/precache-manifest.4341bd7b4b045f492c3ae70739331b5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e7314e78a7c8ddeb81c",
+    "url": "/css/app.e9218c93.css"
+  },
+  {
+    "revision": "5145c739be4fea6d5328",
+    "url": "/css/chunk-vendors.8c8d3bc6.css"
+  },
+  {
+    "revision": "b334b22a80e609a2b3c22a63482451f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e7314e78a7c8ddeb81c",
+    "url": "/js/app.ee13cfa0.js"
+  },
+  {
+    "revision": "5145c739be4fea6d5328",
+    "url": "/js/chunk-vendors.6b6ef663.js"
+  },
+  {
+    "revision": "fe4fd0648294fcc4be4abdce05cf1c4b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
